e8811d61d6caf83602efa9c39e2850e2728c3d33
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-06-19 08:29:43 -0400

    finalize schemas

--- Models/ArtistLike.cs
+++ Models/ArtistLike.cs
@@ -2,10 +2,7 @@ namespace MusicDb.Models {
 
   public class ArtistLike : BaseEntity {
 
-    public long UserId { get; set; }
     public User User { get; set; }
-
-    public long ArtistId { get; set; }
     public Artist Artist { get; set; }
 
   }
--- Models/SongLike.cs
+++ Models/SongLike.cs
@@ -1,13 +1,8 @@
 namespace MusicDb.Models {
 
   public class SongLike : BaseEntity {
-
-    public long UserId { get; set; }
     public User User { get; set; }
-
-    public long ArtistId { get; set; }
     public Song Song { get; set; }
-
   }
 
 }
