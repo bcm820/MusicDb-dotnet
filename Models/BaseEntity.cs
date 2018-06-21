f44993bc38a70a62da868617cf8e76cc7b2d1ae4
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-06-21 08:11:59 -0400

    refactor

--- Models/BaseEntity.cs
+++ Models/BaseEntity.cs
@@ -2,6 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
+/* Useful Annotations and Examples:
+[Required]
+[MinLength(100)]
+[MaxLength(1000)]
+[Range(5,10)] - Field must be between 5 and 10 characters.
+[RegularExpression(@"[0-9]{0,}\.[0-9]{2}", ErrorMessage = "error Message")]
+[EmailAddress] - Field must contain an @ symbol, followed by a word and a period.
+[DataType(DataType.Password)] - Ensures field conforms to specific DataType
+
+Migrations:
+dotnet ef migrations add first - Creates migration. Requires one model in advance.
+dotnet ef database update - Creates and updates DB. */
+
 namespace MusicDb.Models {
 
   public abstract class BaseEntity {
--- Models/User.cs
+++ Models/User.cs
@@ -18,18 +18,3 @@ namespace MusicDb.Models {
   }
 
 }
-
-/*
-Useful Annotations and Examples:
-[Required]
-[MinLength(100)]
-[MaxLength(1000)]
-[Range(5,10)] - Field must be between 5 and 10 characters.
-[RegularExpression(@"[0-9]{0,}\.[0-9]{2}", ErrorMessage = "error Message")]
-[EmailAddress] - Field must contain an @ symbol, followed by a word and a period.
-[DataType(DataType.Password)] - Ensures field conforms to specific DataType
-
-Migrations:
-dotnet ef migrations add FirstMigration - Creates migration. Requires one model in advance.
-dotnet ef database update - Applies migrations much like Django's "migrate" command.
-*/
