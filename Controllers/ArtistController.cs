b2a75c4d4839e2c33b927336613cf31848eb0990
bcmendoza <31445238+bcmendoza@users.noreply.github.com>
2018-06-24 13:23:31 -0400

    split up controllers

--- Controllers/ArtistController.cs
+++ Controllers/ArtistController.cs
@@ -1,126 +1,48 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
-
-using System;
-using System.Threading.Tasks;
-using System.Dynamic;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
 
+using System.Linq;
+using System.Threading.Tasks;
+
+using MusicDb.Services;
 using MusicDb.Models;
 using MusicDb.Extensions;
-using MusicDb.Services;
 
 namespace MusicDb.Controllers {
 
   public class ArtistController : Controller {
 
-    private Context Db;
-    private ApiService Api;
-    public ArtistController(Context context, ApiService api) {
-      Db = context;
-      Api = api;
+    private Context _context;
+    private ArtistService _as;
+
+    public ArtistController(Context context, ArtistService artistService) {
+      _context = context;
+      _as = artistService;
     }
 
-    static List<Song> GetSongsList(dynamic songs, bool results) {
-      var SongsList = new List<Song>();
-      foreach (var song in songs) {
-        var s = results ? song.result : song;
-        SongsList.Add(new Song {
-          Id = s.id,
-          Title = s.title_with_featured,
-          Url = s.url,
-          ArtistName = s.primary_artist.name,
-          ArtistUID = s.primary_artist.id
-        });
-      }
-      return SongsList;
+    [Route("")]
+    public IActionResult Index() {
+      ViewBag.Artists = _context.Artists.OrderBy(artist => artist.Name).ToList();
+      ViewBag.User = HttpContext.Session.GetString("username");
+      return View();
     }
 
-    // Data fetched from API proxy is parsed into an ExpandoObject,
-    // a unique C# type that bypasses compile time type checks,
-    // allowing free access to deep-nested values via dot notation.
-
-    [Route("songs")]
-    async public Task<IActionResult> ShowSearchResults(string text) {
-
-      // Get song data from search API call
-      var ResponseString = await Api.GetSearchResults(text);
-
-      // Parse into ExpandoObject, check for an error
-      dynamic ResponseObj = JsonConvert.DeserializeObject<ExpandoObject>
-        (ResponseString, new ExpandoObjectConverter());
-      if (((IDictionary<string, object>)ResponseObj).ContainsKey("error"))
-        return Json(ResponseObj);
-
-      // Map song data to Song model, map to list, store in session
-      List<Song> Songs = GetSongsList(ResponseObj.response.hits, true);
-      HttpContext.Session.SetDynamic($"search{text}", Songs);
-      return RedirectToAction("Results", "View", new { text = text });
+    [HttpPost]
+    [Route("search")]
+    async public Task<IActionResult> Results(string search) {
+      ViewBag.Results = await _as.ShowSearchResults(search);
+      return View("search");
     }
 
+    // TODO: Fetch Twitter & Instagram data 
     [Route("artists/{id}")]
-    async public Task<IActionResult> ShowArtist(string id) {
-
-      // Check database for artist
-      // If found, redirect to fetch artist's songs
-      var DbArtists = Db.Artists
-        .Where(a => a.Id == Convert.ToInt64(id))
-        .Include(a => a.Likes)
-        .ToList();
-      if (DbArtists.Count != 0) {
-        var FoundArtist = DbArtists.Single();
-        HttpContext.Session.SetDynamic($"artistprofile{id}", FoundArtist);
-        return RedirectToAction("ShowArtistSongs", new { id = id });
-      }
-
-      // Get data from artist API call
-      var ResponseString = await Api.GetArtistInfo(id);
-
-      // Parse into ExpandoObject, check for an error
-      dynamic ResponseObj = JsonConvert.DeserializeObject<ExpandoObject>
-        (ResponseString, new ExpandoObjectConverter());
-      if (((IDictionary<string, object>)ResponseObj).ContainsKey("error"))
-        return Json(ResponseObj);
-
-      // Map artist data to Artist model, store in DB
-      // Then redirect to fetch artist songs
-      var ArtistData = ResponseObj.response.artist;
-      var Artist = new Artist {
-        Id = ArtistData.id,
-        Name = ArtistData.name,
-        Url = ArtistData.url,
-        Image = ArtistData.image_url,
-        Instagram = ArtistData.instagram_name,
-        Twitter = ArtistData.twitter_name
-      };
-      Db.Artists.Add(Artist);
-      Db.SaveChanges();
-      HttpContext.Session.SetDynamic($"artistprofile{id}", Artist);
-      return RedirectToAction("ShowArtistSongs", new { id = id });
-    }
-
-    [Route("artists/{id}/songs")]
-    async public Task<IActionResult> ShowArtistSongs(string id) {
-
-      // Get data from artist's songs API call
-      var ResponseString = await Api.GetArtistSongs(id);
-
-      // Deserialize into ExpandoObject, check for an error
-      dynamic ResponseObj = JsonConvert.DeserializeObject<ExpandoObject>
-        (ResponseString, new ExpandoObjectConverter());
-      if (((IDictionary<string, object>)ResponseObj).ContainsKey("error"))
-        return Json(ResponseObj);
-
-      // Map song data to Song model, map to list, store in session
-      List<Song> Songs = GetSongsList(ResponseObj.response.songs, false);
-      HttpContext.Session.SetDynamic($"artistsongs{id}", Songs);
-
-      return RedirectToAction("Artist", "View", new { id = id });
+    async public Task<IActionResult> Artist(string id) {
+      var Artist = await _as.ShowArtist(id);
+      var Songs = await _as.ShowArtistSongs(id);
+      ViewBag.Artist = Artist;
+      ViewBag.Songs = Songs;
+      return View("artist");
     }
 
   }
--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -11,21 +11,14 @@ using MusicDb.Extensions;
 
 namespace MusicDb.Controllers {
 
-  public class ViewController : Controller {
+  public class UserController : Controller {
 
     private Context _context;
     private ArtistService _as;
 
-    public ViewController(Context context, ArtistService artistService) {
+    public UserController(Context context, ArtistService artistService) {
       _context = context;
       _as = artistService;
-    }
-
-    [Route("")]
-    public IActionResult Index() {
-      ViewBag.Artists = _context.Artists.OrderBy(artist => artist.Name).ToList();
-      ViewBag.User = HttpContext.Session.GetString("username");
-      return View();
     }
 
     [Route("register")] public IActionResult RegisterGet() => View("register");
@@ -42,22 +35,6 @@ namespace MusicDb.Controllers {
       _context.SaveChanges();
       HttpContext.Session.SetString("username", NewUser.Username);
       return RedirectToAction("Index");
-    }
-
-    [Route("results/{text}")]
-    async public Task<IActionResult> Results(string text) {
-      ViewBag.Results = await _as.ShowSearchResults(text);
-      return View("results");
-    }
-
-    // TODO: Fetch Twitter & Instagram data 
-    [Route("artists/{id}/profile")]
-    async public Task<IActionResult> Artist(string id) {
-      var Artist = await _as.ShowArtist(id);
-      var Songs = await _as.ShowArtistSongs(id);
-      ViewBag.Artist = Artist;
-      ViewBag.Songs = Songs;
-      return View("artist");
     }
 
   }
